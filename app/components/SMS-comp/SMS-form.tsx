84c08a1b4ca53e3d89d79a42ed23d57c70c91f8e
NDERI007 <tnderi373@gmail.com>
2025-07-10 13:25:26 +0300

    small lint error

--- app/components/SMS-comp/SMS-form.tsx
+++ app/components/SMS-comp/SMS-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import ContactGroupSelector from './ContactSelcector';
 import axios from 'axios';
 import { CalendarClock } from 'lucide-react';
